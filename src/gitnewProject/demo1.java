69b2d57ecf11abdda8dd90d20fbcc7ac03bf331a
user <user@DESKTOP-G89FQG8>
2022-08-01 12:11:49 +0530

    [ajay]: new

--- src/gitnewProject/demo1.java
+++ src/gitnewProject/demo1.java
@@ -1,11 +1,10 @@
 package gitnewProject;
 
-public class sample1 
+public class demo1 
 {
+ 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello");
+		System.out.println("Hello ");
 	}
 }
-
-
